3f7f6f89494be083d426ebcec8f68d3a3a62fc89
hooded22 <przemek.sipta@gmail.com>
2024-05-31 13:12:09 +0200

    feat: Add prettier

--- prettier.config.js
+++ prettier.config.js
@@ -2,5 +2,10 @@ const styleguide = require('@vercel/style-guide/prettier');
 
 module.exports = {
   ...styleguide,
+  semi: true,
+  trailingComma: 'all',
+  singleQuote: true,
+  printWidth: 120,
+  tabWidth: 2,
   plugins: [...styleguide.plugins, 'prettier-plugin-tailwindcss'],
 };
